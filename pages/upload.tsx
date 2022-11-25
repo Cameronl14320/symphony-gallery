a0e5c3c622dc8c07f9f58558ce05fe4fde19d4a5
Cameron Li <cameronl14320@gmail.com>
2022-11-25 13:56:30 +1300

    Image preview and error messages for uploaded files

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -4,36 +4,64 @@ import React, {useState} from "react";
 
 export const Upload = () => {
     const [uploaded, setUploaded] = useState<File[]>([]);
+    const [error, setError] = useState<string>("");
     const storageRef = ref(firebaseStorage, 'some-child');
 
     const handleDropEvent = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
 
         const items = Array.from(event.dataTransfer.items);
+        const existingImages = uploaded;
         const images: File[] = [];
         if (items) {
             items.forEach((item, index) => {
                 if (item.kind === 'file' && item.type.match('^image/')) {
                     const file = item.getAsFile();
                     if (!!file) {
+                        if (!existingImages.some(image => image != file)) {
+                            images.push(file);
+                        } else {
                         images.push(file);
+                            setError(`${file.name} was already uploaded`);
+                        }
+                    } else {
+                        setError("Invalid file was uploaded");
                     }
+                } else {
+                    setError("File is not a valid image");
                 }
             });
         }
 
-        setUploaded(images);
+        setUploaded(existingImages.concat(images));
     }
 
     const handleDragOverEvent = (event: React.DragEvent<HTMLDivElement>) => {
         event.preventDefault();
     }
 
+    const stomp: any[] = uploaded.map(file => (
+        <div key={file.name}>
+            <img id="test" src={URL.createObjectURL(file)} alt="testa" style={{objectFit: "contain"}}/>
+        </div>
+    ));
+
     return (
         <div>
             Upload something here
-            <div style={{width: "500px", height: "500px", background: "black"}} onDrop={(event) => handleDropEvent(event)} onDragOver={(event) => handleDragOverEvent(event)}>
-
+            <div style={{display: "flex", width: "500px", height: "500px", background: "gray"}} onDrop={(event) => handleDropEvent(event)} onDragOver={(event) => handleDragOverEvent(event)}>
+                <div style={{display: "flex", background: "white", flex: "1 1", margin: "8px"}}>
+                    {stomp}
+                </div>
+            </div>
+            <div>
+                {error}
+            </div>
+            <div>
+            </div>
+            
+            <div>
+                Upload images
             </div>
         </div>
     );
